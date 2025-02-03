153db5309686ee370d261e3218ce2c932fe5639f
Erich Boschert <boschert.12@osu.edu>
2025-02-03 18:29:15 -0500

    Fixing clean behavior

--- src/main/java/com/supasulley/main/InputListener.java
+++ src/main/java/com/supasulley/main/InputListener.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
